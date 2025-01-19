7bac087be9cdc1d44fb918a15aca4ee3aaf7c015
Akash Rawat <akash.rawat@valiancesolutions.com>
2025-01-19 20:32:41 +0530

    API changes

--- src/api/Backend.js
+++ src/api/Backend.js
@@ -1,23 +1,3 @@
-// Cloud run
-const hostname= "https://iocl-backend-j7no7yv4kq-el.a.run.app"
-// localhost
-// const hostname= "http://127.0.0.1:8000"
+const hostname= "http://127.0.0.1:8000"
 
-
-// Python Automation Script (in VM)
-// const botScriptHostname = "http://34.100.143.71"
-const botScriptHostname = "https://iocl-automation.valiancelab.com"
-// const botScriptHostname = "http://127.0.0.1:5000"
-
-// GenAI Backend 
-// ( NEW VM)
-// const genAIBEHostname = "http://34.47.186.5:8000"
-const genAIBEHostname = "https://iocl-be.valiancelab.com"
-// ( OLD VM)
-// const genAIBEHostname = "http://34.47.223.29:8000"
-
-
-// const genAIBEHostname = "https://iocl-be.valiancelab.com"
-
-
-export {hostname, botScriptHostname, genAIBEHostname}
+export { hostname }
--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,38 +1,7 @@
-import {hostname, botScriptHostname, genAIBEHostname} from "./Backend"
+import {hostname} from "./Backend"
 
 const endpoint = {
-    login: `${hostname}/user_profile/login/`,
-    fetchAllTenders: `${hostname}/tenders/all_tenders/`,
-    tenderCount: `${hostname}/tenders/tender_count/`,
-    manualCreateTender: `${hostname}/tenders/create_tender/`,
-    addPqr: `${hostname}/tenders/add_pqrs/`,
-    fetchPQRList: `${hostname}/tenders/tender_pqr`,
-    editPQR: `${hostname}/tenders/edit_pqrs/`,
-    triggerStatus: `${hostname}/tenders/trigger_status/`,
-    triggerGenAI: (docID) => `${genAIBEHostname}/process_tender/${docID}/`,  // Gen AI Backend Trigger
-    tenderStat: (docID) => `${hostname}/tenders/tenders/${docID}`,
-    tenderInfo: (docID) => `${hostname}/tenders/tender_info/${docID}`,
-    bidderDetail: (docID) => `${hostname}/tenders/bidders/${docID}`,
-    uploadTenderDocument: (docID) => `${hostname}/tenders/${docID}/upload/`,
-    uploadNewBidderDocument: (docID) => `${hostname}/tenders/${docID}/create_bidder/`,
-    uploadExistingBidderDocument: (docID) => `${hostname}/tenders/${docID}/update_bidder/`,
-    tenderDetails: (status) => `${hostname}/tenders/tenders_by_status/?status=${status}`,
-    downloadFolder: (url) => `${hostname}/tenders/download-folder/?url=${url}`,
-    triggerBot: `${hostname}/tenders/trigger-bot/`,
-    pqrList: `${hostname}/tenders/pqr_list/`,
-    botScript: `${botScriptHostname}/bot_script`,
-    acceptPqr: (docID) => `${hostname}/tenders/accept_bidder_pqr/${docID}/`,
-    declinePqr: (docID) => `${hostname}/tenders/decline_bidder_pqr/${docID}/`,
-    editPqr: (docID) => `${hostname}/tenders/edit_bidder_pqr/${docID}/`,
-    accept_bidder_all_pqrs: (docID) => `${hostname}/tenders/accept_all_pqrs/${docID}/`,
-    decline_bidder_all_pqrs: (docID) => `${hostname}/tenders/decline_all_pqrs/${docID}/`,
-    accept_all_bidders_all_pqrs: (docID) => `${hostname}/tenders/accept_all_bidder_pqrs/${docID}/`,
-    decline_all_bidders_all_pqrs: (docID) => `${hostname}/tenders/decline_all_bidder_pqrs/${docID}/`,
-    generate_tqs: (docID) => `${hostname}/tenders/generate_tqs/${docID}/`,
-    generate_cs: (docID) => `${hostname}/tenders/generate_comparative_statement/${docID}/`,
-    send_bidder_tqs_email: (docID) => `${hostname}/tenders/send_bidder_tqs_email/${docID}/`,
-    createTenderV2: `${hostname}/tenders/create_tender_v2/`,
-    addFirestoreData: `${hostname}/tenders/add_firestore/`,
+    login: `${hostname}/user_profile/login/`
 }
 
 export default endpoint
